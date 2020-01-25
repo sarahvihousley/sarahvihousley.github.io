fabf0bee0b211eff61ab2e2552cd3071ba3964e5
sarahvihousley <cla17035@byui.edu>
2020-01-24 20:18:11 -0700

    commit

---
+++ lesson4/js/preston.js
@@ -0,0 +1,10 @@
+var d=new Date()
+    var weekday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday",
+                    "Friday","Saturday")
+    var monthname=new Array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug",
+                    "Sep","Oct","Nov","Dec")
+    document.write(weekday[d.getDay()] + " ")
+    document.write(d.getDate() + ". ")
+    document.write(monthname[d.getMonth()] + " ")
+    document.write(d.getFullYear())
+    var p = document.getElementById("day").innerHTML = d;
