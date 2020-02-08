d74b736bda13bf3a183a40257e21c07a2be45464
sarahvihousley <cla17035@byui.edu>
2020-02-07 17:30:03 -0700

    commit

--- lesson5/js/preston.js
+++ lesson5/js/preston.js
@@ -1,10 +1,17 @@
-var d=new Date()
-    var weekday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday",
-                    "Friday","Saturday")
-    var monthname=new Array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug",
-                    "Sep","Oct","Nov","Dec")
-    document.write(weekday[d.getDay()] + " ")
-    document.write(d.getDate() + ", ")
-    document.write(monthname[d.getMonth()] + " ")
-    document.write(d.getFullYear())
-    var p = document.getElementById("day").innerHTML = d;
+function toggleMenu() {
+    document.getElementById("pureNav").classList.toggle("hide");
+}
+var currentDate = new Date();
+var dayNames = ["Sunday","Monday", "Tuesday", "Wednesday", 
+"Thursday", "Friday", "Saturday"];
+var day = new Date().getDay();
+document.getElementById("day1").innerText = dayNames[(day + 1) % 7];
+document.getElementById("day2").innerText = dayNames[(day + 2) % 7];
+document.getElementById("day3").innerText = dayNames[(day + 3) % 7];
+document.getElementById("day4").innerText = dayNames[(day + 4) % 7];
+document.getElementById("day5").innerText = dayNames[(day + 5) % 7];
+if (day != 5) {
+    document.getElementById("alert").style.display = "none";
+}
+var mod = moment(new Date(document.lastModified));
+document.getElementById("date").innerText = mod.format('dddd, DD MMMM YYYY');
